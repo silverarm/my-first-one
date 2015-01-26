09e6ae9d09025140b5d2b4e066d2a1fdb9a0af90
silverarm <areddan@silverarm.com>
2015-01-26 22:49:36 +0000

    just a title change

--- app/views/hello.php
+++ app/views/hello.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Laravel PHP Framework</title>
+	<title>Laravel PHP silverarm Framework</title>
 	<style>
 		@import url(//fonts.googleapis.com/css?family=Lato:700);
 
